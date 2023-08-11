e7f33507e3e74fe08daf10ab6c9843db8733cfd4
xX3N1A <xenia.lainscsek@tugraz.at>
2023-08-11 09:52:57 +0200

    load_cooler.jl done

--- load_cooler.jl
+++ load_cooler.jl
@@ -1,4 +1,4 @@
-using HDF5, DataFrames
+using HDF5, DataFrames, LinearAlgebra, Plots
 
 function load_cooler(FN, ChrNr, Resolution)
 # INPUT VARS
@@ -42,19 +42,27 @@ function load_cooler(FN, ChrNr, Resolution)
 	BINIDs .= BINIDs .- BINIDs[1, 1] .+ 1
 
 	CONTACT_MAP = fill(NaN, size(BIN_TABLE, 1), size(BIN_TABLE, 1))
-	index = LinearIndices(size(CONTACT_MAP), BINIDs[:, 1], BINIDs[:, 2])
-	CONTACT_MAP[index] .= counts
+
+        index = (BINIDs[:, 2] .- 1) .* size(CONTACT_MAP, 1) .+ BINIDs[:, 1]
+        CONTACT_MAP[index] = counts
+
 	CONTACT_MAP .= CONTACT_MAP .* BIN_TABLE.weights
-
-	CONTACT_MAP .= triu(CONTACT_MAP) .+ triu(CONTACT_MAP, 1)
-
-	INCLUDE = unique(findall(sum(isnan.(CONTACT_MAP), dims=2) .< size(CONTACT_MAP, 2)))
-
+	CONTACT_MAP .= triu(CONTACT_MAP) .+ triu(CONTACT_MAP, 1)'
+       	INCLUDE = unique(findall(sum(isnan.(CONTACT_MAP), dims=2) .< size(CONTACT_MAP, 2)))
 	EMPTY_BIN = fill(NaN, size(BIN_TABLE, 1))
 	EMPTY_BIN[INCLUDE] .= 1
 	EMPTY_BIN[isnan.(BIN_TABLE.weights)] .= NaN
 
 	BIN_TABLE[!, :CONTACT] = EMPTY_BIN
 
-	return C_MAP, BIN_TABLE
+	return CONTACT_MAP, BIN_TABLE
+
+        #BIN_TABLE[!,:binNrCHRS] .= 1:size(BIN_TABLE,1)
+        #B = BIN_TABLE[.!isnan.(BIN_TABLE.CONTACT), :]
+        #CONTACT_MAP = CONTACT_MAP[B.binNrCHRS,B.binNrCHRS]
+        #CONTACT_MAP = CONTACT_MAP[600:800,600:800]
+        #gr()# pyplot(), gr(), or plotly()
+        #heatmap(log.(CONTACT_MAP), aspect_ratio = 1, legend = false, color=:jet, axis = :square)
+
+
 end
