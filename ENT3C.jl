a8b8fdac162b0e293cf7f652055556a107fa2447
X3N1A <108679125+xX3N1A@users.noreply.github.com>
2024-03-27 16:33:16 +0100

    mulichromosomal analysis
    specify multiple chromosomes in config file as
    "chr_n:chr_m"
    
    solved julia matlab discrepancy: Julias cor() function --> all diagonal values set to 1, evey in row i and/or j have zero variance

--- ENT3C.jl
+++ ENT3C.jl
@@ -1,13 +1,11 @@
-using DataFrames, BenchmarkTools, JSON, Printf
+using DataFrames, BenchmarkTools, JSON, Printf, Plots, ColorSchemes
 
 include("JULIA_functions/ent3c_functions.jl")
 
 config = JSON.parsefile("config/config.json")
 
 SUB_M_SIZE_FIX = config["SUB_M_SIZE_FIX"]
-ChrNr = config["ChrNr"]
 WN_MAX = config["WN_MAX"]
-CHRSPLIT = config["CHRSPLIT"]
 Resolution = config["Resolution"]
 WS = config["WS"]
 NormM = config["NormM"]
@@ -15,8 +13,10 @@ DATA_PATH = config["DATA_PATH"]
 FILES = config["FILES"]
 FILES = hcat(joinpath.(DATA_PATH, FILES[1:2:end-1]), FILES[2:2:end])
 OUT_PREFIX = config["OUT_PREFIX"]
+CHRSPLIT = config["CHRSPLIT"]
+
 if OUT_PREFIX === nothing
-    OUT_PREFIX = @sprintf("Chr%d_%dkb",ChrNr,Resolution/1e3)
+    OUT_PREFIX = @sprintf("%dkb",Resolution/1e3)
 end
 OUT_DIR = config["OUT_DIR"]
 OUT_DIR = string(OUT_DIR,"JULIA")
@@ -24,9 +24,25 @@ if !isdir(OUT_DIR)
     mkpath(OUT_DIR)
 end
 
-ENT3C_OUT, Similarity, p = main(FILES,Resolution,ChrNr,SUB_M_SIZE_FIX,CHRSPLIT,WN_MAX,WS)
+if contains(config["ChrNr"],":")
+        ChrNrs_1, ChrNrs_2 = parse.(Int, split(config["ChrNr"], ':'))
+        ChrNrs = ChrNrs_1:ChrNrs_2
+    else
+        ChrNrs = parse.(Int, config["ChrNr"])
+end
 
-savefig(p,@sprintf("%s/%s_ENT3C_OUT.png",OUT_DIR,OUT_PREFIX))
+#############################################################################
+# ENT3C table
+##############################################################################
+ENT3C_OUT = main(FILES,Resolution,ChrNrs,SUB_M_SIZE_FIX,CHRSPLIT,WN_MAX,WS,NormM)
+#ENT3C_OUT =  CSV.File(@sprintf("%s/%s_ENT3C_OUT.csv",OUT_DIR,OUT_PREFIX)) |> DataFrame
+
+#############################################################################
+# similarity table
+##############################################################################
+Similarity, PLT = get_similarity_table(ENT3C_OUT)
+
+savefig(PLT,@sprintf("%s/%s_ENT3C_OUT.png",OUT_DIR,OUT_PREFIX))
 CSV.write(@sprintf("%s/%s_ENT3C_OUT.csv",OUT_DIR,OUT_PREFIX), ENT3C_OUT)
 CSV.write(@sprintf("%s/%s_ENT3C_similarity.csv",OUT_DIR,OUT_PREFIX), Similarity)
-
+	
