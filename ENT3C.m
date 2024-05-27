8934f5eeda8a2df4cdbf92204a871d0f5c4dadfc
X3N1A <108679125+X3N1A@users.noreply.github.com>
2024-05-27 17:13:49 +0200

    testing config giles

--- ENT3C.m
+++ ENT3C.m
@@ -3,7 +3,7 @@ addpath('MATLAB_functions/')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % load json file
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-config = fileread('config/config.pooledBRs.json');
+config = fileread('config/config.json');
 config = jsondecode(config);
 
 WN_MAX=config.WN_MAX;
@@ -70,8 +70,8 @@ for Resolution=Resolutions
             M = M(INCLUDE,INCLUDE);
             BIN_TABLE = BIN_TABLE(INCLUDE,:);
 
-            [S, SUB_M_SIZE1, WN1, WS1, BIN_TABLE_NEW] = vN_entropy(M,SUB_M_SIZE_FIX,CHRSPLIT,WN_MAX,WS,BIN_TABLE);
 
+            [S, SUB_M_SIZE1, WN1, WS1, BIN_TABLE_NEW] = vN_entropy(M,SUB_M_SIZE_FIX,CHRSPLIT,WN_MAX,WS,BIN_TABLE);
             N = length(S);
 
             OUT1 = table(repmat({FNs(f).META},N,1),...
@@ -94,63 +94,64 @@ writetable(ENT3C_OUT,sprintf('%s/%s_ENT3C_OUT.csv',OUT_DIR,OUT_PREFIX),'Delimite
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % fill similarity table
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SAMPLES=unique(ENT3C_OUT.Name);
 
 Similarity = [];
+if numel(SAMPLES)>1
 
-for Resolution=Resolutions
-    figure(Resolution);hold on;tiledlayout('flow');hold on
-    for ChrNr=ChrNrs
-        figure(Resolution);nexttile
+    for Resolution=Resolutions
+        figure(Resolution);hold on;tiledlayout('flow');hold on
+        for ChrNr=ChrNrs
+            figure(Resolution);nexttile
 
-        SAMPLES=unique(ENT3C_OUT.Name);
-        comparisons = get_pairwise_combs(SAMPLES);
-        plotted = "tempstring";
-        c=1;
-        for f=1:size(comparisons,1)
+            comparisons = get_pairwise_combs(SAMPLES);
+            plotted = "tempstring";
+            c=1;
+            for f=1:size(comparisons,1)
 
-            S1 = ENT3C_OUT(strcmp(ENT3C_OUT.Name,comparisons{f,1})&ENT3C_OUT.ChrNr==ChrNr&ENT3C_OUT.Resolution==Resolution,:);
-            S2 = ENT3C_OUT(strcmp(ENT3C_OUT.Name,comparisons{f,2})&ENT3C_OUT.ChrNr==ChrNr&ENT3C_OUT.Resolution==Resolution,:);
-            Q = corrcoef(S1.S,S2.S);Q=Q(1,2);
+                S1 = ENT3C_OUT(strcmp(ENT3C_OUT.Name,comparisons{f,1})&ENT3C_OUT.ChrNr==ChrNr&ENT3C_OUT.Resolution==Resolution,:);
+                S2 = ENT3C_OUT(strcmp(ENT3C_OUT.Name,comparisons{f,2})&ENT3C_OUT.ChrNr==ChrNr&ENT3C_OUT.Resolution==Resolution,:);
+                Q = corrcoef(S1.S,S2.S);Q=Q(1,2);
 
-            Similarity=[Similarity;...
-                table(Resolution,ChrNr,comparisons{f,1},comparisons{f,2},Q,...
-                'VariableNames',{'Resolution','ChrNr','Sample1','Sample2','Q'})];
+                Similarity=[Similarity;...
+                    table(Resolution,ChrNr,comparisons{f,1},comparisons{f,2},Q,...
+                    'VariableNames',{'Resolution','ChrNr','Sample1','Sample2','Q'})];
 
-            if ~ismember(comparisons{f,1}, plotted)
-                figure(Resolution);plot(S1.S,'color',CM(c,:),'linewidth',1.3);hold on;axis tight
-                plotted = [plotted;comparisons{f,1}];c=c+1;
+                if ~ismember(comparisons{f,1}, plotted)
+                    figure(Resolution);plot(S1.S,'color',CM(c,:),'linewidth',1.3);hold on;axis tight
+                    plotted = [plotted;comparisons{f,1}];c=c+1;
+                end
+                if ~ismember(comparisons{f,2}, plotted)
+                    figure(Resolution);plot(S2.S,'color',CM(c,:),'linewidth',1.3);hold on;axis tight
+                    plotted = [plotted;comparisons{f,2}];c=c+1;
+                end
             end
-            if ~ismember(comparisons{f,2}, plotted)
-                figure(Resolution);plot(S2.S,'color',CM(c,:),'linewidth',1.3);hold on;axis tight
-                plotted = [plotted;comparisons{f,2}];c=c+1;
-            end
-        end
-        if Biologicap_replicates
-            title(sprintf('Chr%d %dkb $\\overline{Q}_{BR}=%4.2f$ $\\overline{Q}_{nonBR}=%4.2f$',...
-                ChrNr,Resolution/1e3,...
-                mean(Similarity.Q(Similarity.ChrNr==ChrNr&Similarity.Resolution==Resolution&...
+            if Biologicap_replicates
+                title(sprintf('Chr%d %dkb $\\overline{Q}_{BR}=%4.2f$ $\\overline{Q}_{nonBR}=%4.2f$',...
+                    ChrNr,Resolution/1e3,...
+                    mean(Similarity.Q(Similarity.ChrNr==ChrNr&Similarity.Resolution==Resolution&...
                     strcmp(extractBefore(Similarity.Sample1,'_'),extractBefore(Similarity.Sample2,'_')))),...
-                mean(Similarity.Q(Similarity.ChrNr==ChrNr&Similarity.Resolution==Resolution&...
+                    mean(Similarity.Q(Similarity.ChrNr==ChrNr&Similarity.Resolution==Resolution&...
                     ~strcmp(extractBefore(Similarity.Sample1,'_'),extractBefore(Similarity.Sample2,'_'))))),...
-                'interpreter','latex','fontsize',12)
-        else
-            title(sprintf('Chr%d %dkb',ChrNr,Resolution/1e3),'interpreter','latex','fontsize',12)
-        end
+                    'interpreter','latex','fontsize',12)
+            else
+                title(sprintf('Chr%d %dkb',ChrNr,Resolution/1e3),'interpreter','latex','fontsize',12)
+            end
 
-        if ChrNr==ChrNrs(end)
-            L = arrayfun(@(s) setfield(s, 'META', strrep(s.META, '_', ' ')), FNs);
-            legend(L.META,'location','northeastoutside')
+            if ChrNr==ChrNrs(end)
+                L = arrayfun(@(s) setfield(s, 'META', strrep(s.META, '_', ' ')), FNs);
+                legend(L.META,'location','northeastoutside')
+            end
+            set(gca,'FontSize',20)
+
         end
-        set(gca,'FontSize',20)
+        figure(Resolution);
+        set(gcf,'Position',[52 215 1717 813])
+        saveas(gcf,sprintf('%s/%s_%d_ENT3C_signals.svg',OUT_DIR,OUT_PREFIX,Resolution))
 
     end
-    figure(Resolution);
-    set(gcf,'Position',[52 215 1717 813])
-    saveas(gcf,sprintf('%s/%s_%d_ENT3C_signals.svg',OUT_DIR,OUT_PREFIX,Resolution))
-
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % save output
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    writetable(Similarity,sprintf('%s/%s_ENT3C_similarity.csv',OUT_DIR,OUT_PREFIX),'Delimiter','tab')
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% save output
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-writetable(Similarity,sprintf('%s/%s_ENT3C_similarity.csv',OUT_DIR,OUT_PREFIX),'Delimiter','tab')
-
